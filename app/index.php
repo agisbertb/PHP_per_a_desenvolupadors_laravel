592095322f9c7bd47bcef365aaab1b97988626d1
agisbertb <agisbert@iesebre.com>
2022-10-26 11:00:44 +0200

    Connexió amb la base de dades

--- app/index.php
+++ app/index.php
@@ -1,3 +1,17 @@
 <?php
 
-$greting = 'Hola mon!';
+// QUERY STRING
+//var_dump($_GET['name']);
+//$name = 'Andreu';
+
+// API
+function greet(){
+    $name = htmlspecialchars($_GET['name']);
+    $surname = $_GET['surname'];
+
+    echo "Hola $name $surname!";
+}
+
+$greeting = greet();
+
+//$greting = 'Hola' . $_GET['name'] . '' . $_GET['name'] . '!';
