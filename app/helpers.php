a47d43522d445b279646c6b0beeafd39935d4a44
agisbertb <agisbert@iesebre.com>
2022-10-31 18:03:53 +0100

    Afegida la funció connect

--- app/helpers.php
+++ app/helpers.php
@@ -11,5 +11,16 @@ function dd($xivato)
 {
     var_dump($xivato);
     die();
+}
+// API
+function connectDB ($config){ //Dependendy Injection
+    try{
+        return new PDO(
+            $config['database']['databasetype'] . ':host=' . $config ['database']['host'] . ';dbname=' . $config['database']['name'],
+            $config ['database']['user'],
+            $config ['database']['password']);
 
+    }catch (\Exception $e){
+        echo 'Error de connexió a la base de dades';
+    }
 }
