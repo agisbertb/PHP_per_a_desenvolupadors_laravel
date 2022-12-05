f4520badc566808eb28e49f0597435100ae004dc
agisbertb <agisbert@iesebre.com>
2022-12-05 17:12:26 +0100

    Primera versió per acabar del encaminador o Router

--- app/Http/Controllers/index.php
+++ app/Http/Controllers/index.php
@@ -1,8 +1,13 @@
 <?php
 
+
 use Framework\App;
 
 $tasks = App::get('database')->selectALL('tasks');
 //$tasks = Tasks::selectAll('tasks'); Laravel Eloquent
 
 $greeting = greet();
+
+require 'resources/views/index.blade.php';
+
+
---
+++ framework/Route.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Framework;
+
+// NO -> $routes->routes -> Getter setters
+// SI -> SET $routes->define($routes)
+use http\Exception\RuntimeException;
+
+class Route
+{
+    protected $routes = [];
+
+    public function register($route)
+    {
+        $this->routes[] = $route;
+    }
+
+    // CHAINING METHODS
+    public function define($routes)
+    {
+        $this->routes = $routes;
+        return $this;
+    }
+
+    public function redirect($uri)
+    {
+        if (!array_key_exists($uri,$this->routes)){
+            require 'resources/views/errors/404.php';
+            return $this;
+        }
+        if (!file_exists($this->routes[$uri])){
+
+//            throw new \RuntimeException("No s'ha trobat el controlador" . $this->routes[$uri]);
+            dd("No s'ha trobat el controlador" . $this->routes[$uri]);
+        }
+        require $this->routes[$uri];
+        return $this;
+    }
+}
