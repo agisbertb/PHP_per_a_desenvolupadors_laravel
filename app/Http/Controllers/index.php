0f61b8cd2b0b7916a873ac19ffb43a599738e513
agisbertb <agisbert@iesebre.com>
2022-12-19 15:42:30 +0100

    Refactorització de les vistes utilitzant layout. Codi DRY

--- app/Http/Controllers/index.php
+++ app/Http/Controllers/index.php
@@ -1,13 +1,9 @@
 <?php
 
-
 use Framework\App;
 
 $tasks = App::get('database')->selectALL('tasks');
-//$tasks = Tasks::selectAll('tasks'); Laravel Eloquent
 
 $greeting = greet();
 
-require 'resources/views/index.blade.php';
-
-
+require '../resources/views/index.blade.php';
---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<?php require '../resources/views/layout/html_header.blade.php';?>
+
+<?php require '../resources/views/layout/nav.blade.php';?>
+
+<div class="min-h-full">
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">ABOUT</h1>
+        </div>
+    </header>
+    <main>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ex mollitia necessitatibus quisquam repudiandae. A animi deleniti distinctio, maiores, modi quas quisquam ratione rerum temporibus tenetur veniam voluptate voluptatibus voluptatum.
+    </main>
+</div>
+
+<?php require '../resources/views/layout/footer.blade.php';?>
+
