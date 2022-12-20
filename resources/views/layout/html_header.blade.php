85f299fd806a15ab46b948ffe0b7c59a083cbee7
agisbertb <agisbert@iesebre.com>
2022-12-20 11:54:57 +0100

    Petits canvis a la solució final

--- resources/views/layout/html_header.blade.php
+++ resources/views/layout/html_header.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>andreugisbert.me</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="icon" type="image/svg+xml" sizes="32x32" href="mark.svg">
 </head>
